d16c0e3fc3d945fabea08600b107f07c977a8d2c
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-07-15 18:21:02 -0400

    v

--- components/grid/gridSite.tsx
+++ components/grid/gridSite.tsx
@@ -11,12 +11,11 @@ interface Grid {
   // data: Category[] | Section[] | Featured[] | Item[] | IMark[];
 }
 export const GridSite: FC<Grid> = ({ data, responsive }) => {
-  const { pathname } = useRouter();
   return (
     <Main>
       <div className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 col-span-5 gap-3 md:gap-6`}>
         {data.map((d, i) => (
-          <CardSite url={pathname} data={d} key={i} />
+          <CardSite data={d} key={i} />
         ))}
       </div>
     </Main>
